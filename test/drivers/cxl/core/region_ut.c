840727c583ff871cf73755922c083ecb80e1737e
Jim Harris <jim.harris@samsung.com>
2023-11-22 19:46:46 +0000

    Add test for cxl_region_setup_targets().
    Signed-off-by: Jim Harris <jim.harris@samsung.com>

--- test/drivers/cxl/core/region_ut.c
+++ test/drivers/cxl/core/region_ut.c
@@ -98,23 +98,26 @@ cxl_port_setup_targets_test(void)
 {
 	struct cxl_port			port;
 	struct cxl_region		region;
-	struct cxl_endpoint_decoder	ep_decoder;
-	struct cxl_port			ep_decoder_port;
+	struct cxl_endpoint_decoder	ep_decoder, ep_decoder2;
+	struct cxl_port			ep_decoder_port, ep_decoder_port2;
 	struct cxl_region_ref		region_ref;
 	struct cxl_port			parent_port;
 	struct cxl_switch_decoder	switch_decoder;
 	struct cxl_root_decoder		root_decoder;
 	struct resource			resource;
-	struct cxl_memdev		memdev;
-	struct cxl_ep			ep;
+	struct cxl_memdev		memdev, memdev2;
+	struct cxl_ep			ep, ep2;
 	struct cxl_dport		dport;
 
 	ep_decoder.cxld.dev.parent = &ep_decoder_port.dev;
+	ep_decoder.pos = 0;
 	xa_init(&port.regions);
 	radix_tree_init();
 	CU_ASSERT(xa_insert(&port.regions, (unsigned long)&region, &region_ref, GFP_KERNEL) == 0);
 	region_ref.nr_targets = 2;
 	port.dev.parent = &parent_port.dev;
+	region_ref.region = &region;
+	region_ref.port = &port;
 	region_ref.decoder = &switch_decoder.cxld;
 	region.dev.parent = &root_decoder.cxlsd.cxld.dev;
 	region.params.interleave_granularity = 256;
@@ -123,11 +126,88 @@ cxl_port_setup_targets_test(void)
 	resource.start = 0x1000000000;
 	resource.end =   0x2000000000;
 	region.params.res = &resource;
+	region.params.nr_targets = 2;
+	region.params.targets[0] = &ep_decoder;
+	region.params.targets[1] = &ep_decoder2;
 	ep_decoder_port.uport_dev = &memdev.dev;
 	xa_init(&port.endpoints);
 	ep.dport = &dport;
 	CU_ASSERT(xa_insert(&port.endpoints, (unsigned long)&memdev, &ep, GFP_KERNEL) == 0);
 	CU_ASSERT(cxl_port_setup_targets(&port, &region, &ep_decoder) == 0);
+	CU_ASSERT(region_ref.nr_targets_set == 1);
+	CU_ASSERT(switch_decoder.target[0] == &dport);
+
+	ep_decoder2.pos = 1;
+	ep_decoder2.cxld.dev.parent = &ep_decoder_port2.dev;
+	ep_decoder_port2.uport_dev = &memdev2.dev;
+	ep2.dport = &dport;
+	CU_ASSERT(xa_insert(&port.endpoints, (unsigned long)&memdev2, &ep2, GFP_KERNEL) == 0);
+	CU_ASSERT(cxl_port_setup_targets(&port, &region, &ep_decoder2) == 0);
+	CU_ASSERT(region_ref.nr_targets_set == 1);
+	CU_ASSERT(switch_decoder.target[0] == &dport);
+	CU_ASSERT(switch_decoder.target[1] == NULL);
+}
+
+static void
+cxl_region_setup_targets_test(void)
+{
+	struct cxl_port			port;
+	struct cxl_region		region;
+	struct cxl_endpoint_decoder	ep_decoder, ep_decoder2;
+	struct cxl_port			ep_decoder_port, ep_decoder_port2;
+	struct cxl_region_ref		region_ref;
+	struct cxl_port			parent_port;
+	struct cxl_switch_decoder	switch_decoder;
+	struct cxl_root_decoder		root_decoder;
+	struct resource			resource;
+	struct cxl_memdev		memdev, memdev2;
+	struct cxl_ep			ep, ep2;
+	struct cxl_dport		dport;
+	struct cxl_dev_state		dev_state, dev_state2;
+
+	ep_decoder.cxld.dev.parent = &ep_decoder_port.dev;
+	ep_decoder.pos = 0;
+	xa_init(&port.regions);
+	radix_tree_init();
+	CU_ASSERT(xa_insert(&port.regions, (unsigned long)&region, &region_ref, GFP_KERNEL) == 0);
+	region_ref.nr_targets = 2;
+	port.dev.parent = &parent_port.dev;
+	region_ref.region = &region;
+	region_ref.port = &port;
+	region_ref.decoder = &switch_decoder.cxld;
+	region.dev.parent = &root_decoder.cxlsd.cxld.dev;
+	region.params.interleave_granularity = 256;
+	root_decoder.cxlsd.cxld.interleave_ways = 2;
+	switch_decoder.nr_targets = 2;
+	resource.start = 0x1000000000;
+	resource.end =   0x2000000000;
+	region.params.res = &resource;
+	region.params.nr_targets = 2;
+	region.params.targets[0] = &ep_decoder;
+	region.params.targets[1] = &ep_decoder2;
+	ep_decoder_port.uport_dev = &memdev.dev;
+	ep_decoder_port.dev.parent = &port.dev;
+	ep_decoder_port2.dev.parent = &port.dev;
+	memdev.cxlds = &dev_state;
+	memdev2.cxlds = &dev_state2;
+	dev_state.rcd = 0;
+	dev_state2.rcd = 0;
+	xa_init(&port.endpoints);
+	ep.dport = &dport;
+
+	ep_decoder2.pos = 1;
+	ep_decoder2.cxld.dev.parent = &ep_decoder_port2.dev;
+	ep_decoder_port2.uport_dev = &memdev2.dev;
+	ep2.dport = &dport;
+
+	CU_ASSERT(xa_insert(&port.endpoints, (unsigned long)&memdev, &ep, GFP_KERNEL) == 0);
+	CU_ASSERT(xa_insert(&port.endpoints, (unsigned long)&memdev2, &ep2, GFP_KERNEL) == 0);
+
+	CU_ASSERT(cxl_region_setup_targets(&region) == 0);
+
+	CU_ASSERT(region_ref.nr_targets_set == 1);
+	CU_ASSERT(switch_decoder.target[0] == &dport);
+	CU_ASSERT(switch_decoder.target[1] == NULL);
 }
 
 int
@@ -145,6 +225,7 @@ main(int argc, char **argv)
 	CU_ADD_TEST(suite, is_cxl_pmem_region_test);
 	CU_ADD_TEST(suite, interleave_ways_store_test);
 	CU_ADD_TEST(suite, cxl_port_setup_targets_test);
+	CU_ADD_TEST(suite, cxl_region_setup_targets_test);
 
 	CU_basic_set_mode(CU_BRM_VERBOSE);
 	CU_basic_run_tests();
